0e859363f97c625ced1afe252c11edb8ff58aca3
Cory Nelson <fullnelsontech@gmail.com>
2022-09-05 22:39:12 -0400

    Only change was views/todos.ejs where I added correct ejs syntax to pull date data

--- routes/main.js
+++ routes/main.js
@@ -1,15 +1,25 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const authController = require('../controllers/auth') 
-const homeController = require('../controllers/home')
-const { ensureAuth, ensureGuest } = require('../middleware/auth')
-const { uploadProfilePic, uploadErrorHandler, formatUserPhoto } = require('../middleware/multer')
+const authController = require("../controllers/auth")
+const homeController = require("../controllers/home")
+const { ensureAuth, ensureGuest } = require("../middleware/auth")
+const {
+  uploadProfilePic,
+  uploadErrorHandler,
+  formatUserPhoto,
+} = require("../middleware/multer")
 
-router.get('/', homeController.getIndex)
-router.get('/login', authController.getLogin)
-router.post('/login', authController.postLogin)
-router.get('/logout', authController.logout)
-router.get('/signup', authController.getSignup)
-router.post('/signup', uploadProfilePic, uploadErrorHandler, formatUserPhoto, authController.postSignup)
+router.get("/", homeController.getIndex)
+router.get("/login", authController.getLogin)
+router.post("/login", authController.postLogin)
+router.get("/logout", authController.logout)
+router.get("/signup", authController.getSignup)
+router.post(
+  "/signup",
+  uploadProfilePic,
+  uploadErrorHandler,
+  formatUserPhoto,
+  authController.postSignup
+)
 
 module.exports = router
